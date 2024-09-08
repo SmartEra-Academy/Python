1728390feec6ca3fe810e40301e07585322555e0
DanialSoleimany <kingofbbx@gmail.com>
2024-09-09 00:29:25 +0330

    float() Function

---
+++ Type Casting/float_func.py
@@ -0,0 +1,11 @@
+# float() -> Converts a value into a floating-point number
+
+# Converting an integer to a float
+num = 5
+num_float = float(num)
+print(num_float, type(num_float))  
+
+# Converting a string to a float
+num_str = "10.5"
+num_float_2 = float(num_str)
+print(num_float_2, type(num_float_2))  
