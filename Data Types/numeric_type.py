3a59719162395eccea8eca863bf4251dd266c3e9
DanialSoleimany <kingofbbx@gmail.com>
2024-09-08 14:17:39 +0330

    Numeric Type

---
+++ Data Types/numeric_type.py
@@ -0,0 +1,31 @@
+# Numeric Data Types
+
+# integer (int)
+num_1 = 5
+print(num_1, type(num_1))
+
+num_2 = -10
+print(num_2, type(num_2))
+
+num_3 = 0
+print(num_3, type(num_3))
+
+# float 
+num_4 = 4.3
+print(num_4, type(num_4))
+
+num_5 = -3.3
+print(num_5, type(num_5))
+
+num_6 = 0.7
+print(num_6, type(num_6))
+
+num_7 = 2.0 
+print(num_7, type(num_7))
+
+ # complex 
+num_8 = 1 + 3j
+print(num_8, type(num_8))
+
+print(num_8.real) # real part
+print(num_8.imag) # imaginary part
