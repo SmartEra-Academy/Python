a471dbfea71dc1d1d17e41846f6c63192dedb095
DanialSoleimany <kingofbbx@gmail.com>
2024-09-09 00:48:35 +0330

    tuple() Function

---
+++ Type Casting/tuple_func.py
@@ -0,0 +1,11 @@
+# tuple() -> Converts an iterable (like a list or string) into a tuple.
+
+# Converting a list to a tuple
+my_list = [1, 2, 3]
+my_tuple = tuple(my_list)
+print(my_tuple, type(my_tuple)) 
+
+# Converting a string to a tuple
+my_string = "hello"
+my_tuple_2 = tuple(my_string)
+print(my_tuple_2, type(my_tuple_2)) 
