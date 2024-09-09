d81129a17a5e1071af066614a85a26fc0605d375
DanialSoleimany <kingofbbx@gmail.com>
2024-09-09 10:56:43 +0330

    Comparison Operators

---
+++ Operators/comparison_operators.py
@@ -0,0 +1,12 @@
+# Comparison operators are used to compare two values or variables. 
+# The result is a Boolean value
+
+a = 5
+b = 3
+
+print(a == b)  # Equal to
+print(a != b)  # Not equal to
+print(a > b)   # Greater than
+print(a < b)   # Less than
+print(a >= b)  # Greater than or equal to
+print(a <= b)  # Less than or equal to
