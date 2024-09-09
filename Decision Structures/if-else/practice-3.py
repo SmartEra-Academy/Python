bfb2a94b4883bde132b41204e704e408b8e32e79
DanialSoleimany <kingofbbx@gmail.com>
2024-09-09 17:18:21 +0330

    if-else statement & practices

---
+++ Decision Structures/if-else/practice-3.py
@@ -0,0 +1,6 @@
+password = input("Enter your password: ")
+
+if password == "secure123":
+    print("Access granted.")
+else:
+    print("Access denied. Incorrect password.")
---
+++ Decision Structures/if-else/practice-4.py
@@ -0,0 +1,6 @@
+is_raining = input("Is it raining? (yes/no): ")
+
+if is_raining == "yes":
+    print("Take an umbrella!")
+else:
+    print("Enjoy your day without an umbrella.")
