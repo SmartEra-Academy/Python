11d05cf09a2ed07ff483455d448f9dc8acf10d0b
DanialSoleimany <kingofbbx@gmail.com>
2024-09-09 20:29:16 +0330

    for loop, break - continue - pass

---
+++ Repetition Structures/break continue pass/for loop/use-break.py
@@ -0,0 +1,5 @@
+for i in range(10):
+    if i == 5:
+        print("Loop stopped at", i)
+        break  
+    print(i)
---
+++ Repetition Structures/break continue pass/for loop/use-pass.py
@@ -0,0 +1,4 @@
+for i in range(5):
+    if i == 3:
+        pass  # Do nothing when i is 3
+    print(i)
