9004ed497d629e1a3ce1045f59280dc69a173195
DanialSoleimany <kingofbbx@gmail.com>
2024-09-09 15:02:43 +0330

    Logical Operators

---
+++ Operators/logical_operators.py
@@ -0,0 +1,38 @@
+# Logical operators 
+
+# and 
+a = 5
+b = 10
+
+result_1 = a > 3 and b < 15  
+print(result_1)  
+
+result_2 = a > 6 and b == 10 
+print(result_2) 
+
+print(True and True and False and True)
+
+# or
+c = 5
+d = 10
+
+result_3 = c >= 6 or d < 15 
+print(result_3)  
+
+result_4 = c == 6 or d < 5
+print(result_4)  
+
+print(False or False or False or True or False) 
+
+# not
+e = 5
+
+result_5 = not e > 3 
+print(result_5) 
+
+result_6 = not e > 6  
+print(result_6) 
+
+
+
+
