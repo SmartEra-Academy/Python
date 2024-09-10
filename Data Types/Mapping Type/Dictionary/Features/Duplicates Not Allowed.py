c0bbd88ce44e30130545d128d34f543d1b97c2ff
DanialSoleimany <kingofbbx@gmail.com>
2024-09-10 11:12:12 +0330

    Dictionary Features

---
+++ Data Types/Mapping Type/Dictionary/Features/Duplicates Not Allowed.py
@@ -0,0 +1,9 @@
+student_info = {
+    "name":"ali",
+    "city":"esfahan",
+    "age":22,
+    "age":23,
+    "age":24
+}
+
+print(student_info)
---
+++ Data Types/Mapping Type/Dictionary/Features/Mutable.py
@@ -0,0 +1,9 @@
+my_dict = {
+    "name": "saeed",
+    "city": "tehran"
+}
+
+my_dict["name"] = "reza"
+my_dict["age"] = 30
+
+print(my_dict)
