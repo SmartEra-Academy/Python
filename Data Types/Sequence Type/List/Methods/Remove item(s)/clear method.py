997005a57a3b0514faafcea73a28b7005a6ddf5e
DanialSoleimany <kingofbbx@gmail.com>
2024-09-10 04:57:52 +0330

    List Methods to Remove Item(s)

---
+++ Data Types/Sequence Type/List/Methods/Remove item(s)/clear method.py
@@ -0,0 +1,5 @@
+# Remove all items from the list
+
+fruits = ["apple", "banana", "cherry"]
+fruits.clear()
+print(fruits) 
---
+++ Data Types/Sequence Type/List/Methods/Remove item(s)/remove method.py
@@ -0,0 +1,5 @@
+# Remove the first occurrence of the specified item
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+print(fruits)
