b6437523086fad1bd86f1a341a754a9864c8bf3e
DanialSoleimany <kingofbbx@gmail.com>
2024-09-11 13:03:00 +0330

    Lambda Concept & Practices

---
+++ Function/Lambda/lambda_func.py
@@ -0,0 +1,9 @@
+# Regular function 
+def add(x, y):
+    return x + y
+
+print(add(2, 3))
+
+# Lambda function 
+add_lambda = lambda x, y: x + y
+print(add_lambda(2, 3))  
---
+++ Function/Lambda/practice-6.py
@@ -0,0 +1,2 @@
+max_value = lambda a, b: a if a > b else b
+print(max_value(10, 20)) 
