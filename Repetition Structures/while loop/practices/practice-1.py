b57849ce3407b855bdf7a48cf520b3b3f6bb9b37
DanialSoleimany <kingofbbx@gmail.com>
2024-09-14 18:48:35 +0330

    while loop, practice-1.py updated

--- Repetition Structures/while loop/practices/practice-1.py
+++ Repetition Structures/while loop/practices/practice-1.py
@@ -1,6 +1,8 @@
-password = ""
+password = input("Enter the correct password: ")
 
 while password != "secret":
+
+    print("Password is wrong")
     password = input("Enter the correct password: ")
 
 print("Access granted!")
