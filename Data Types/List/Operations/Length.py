ca25e858d1fc18ea9e7c4a6f1459adf1b404e203
DanialSoleimany <kingofbbx@gmail.com>
2024-09-19 11:08:46 +0330

    List Operations

---
+++ Data Types/List/Operations/Length.py
@@ -0,0 +1,5 @@
+# You can use the len() function 
+# to find the number of elements in a list.
+
+my_list = [1, 2, 3, 4, 1, 2, 3, 5]
+print(len(my_list))  
---
+++ Data Types/List/Operations/Repetition.py
@@ -0,0 +1,5 @@
+# You can repeat lists using the * operator
+
+my_list = [1, 2]
+result = my_list * 4
+print(result) 
